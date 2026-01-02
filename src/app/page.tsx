85ae413f13e4fee5b75cf4ecbdb534fa0a77897f
Thalyson Alves <fimon1010@gmail.com>
2026-01-02 09:43:12 +0000

    Combinado! Vou ajustar o carrossel de depoimentos para que ele mude auto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -60,7 +60,7 @@ const authorImage = PlaceHolderImages.find((img) => img.id === 'author-photo');
 export default function HomePage() {
   const [currentDate, setCurrentDate] = useState('');
    const plugin = useRef(
-    Autoplay({ delay: 5000, stopOnInteraction: true })
+    Autoplay({ delay: 3000, stopOnInteraction: true })
   )
 
   useEffect(() => {
