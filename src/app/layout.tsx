64d9411f2326b45a5b99dff793afcc1b8e517648
Thalyson Alves <fimon1010@gmail.com>
2026-01-03 22:01:03 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="https://i.postimg.cc/1XnHBPBD/Inserir-um-titulo.png" sizes="any" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
