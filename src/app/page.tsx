5acb57409c299da695e58c0b948eab3d5b7f4a00
Thalyson Alves <fimon1010@gmail.com>
2026-01-05 20:33:18 +0000

    TIRE ESSE SCRIPT EU MANDEI ERRADO SE USAR ELE VAI DAR PROBLEMA

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -116,32 +116,7 @@ export default function HomePage() {
 
   return (
     <div className="flex flex-col min-h-screen">
-      <Script id="utimify-back-redirect">
-        {`
-          const link = 'https://meubackredirect.com.br';
-
-          function setBackRedirect(url) {
-            let urlBackRedirect = url;
-            urlBackRedirect = urlBackRedirect =
-              urlBackRedirect.trim() +
-              (urlBackRedirect.indexOf('?') > 0 ? '&' : '?') +
-              document.location.search.replace('?', '').toString();
-
-            history.pushState({}, '', location.href);
-            history.pushState({}, '', location.href);
-            history.pushState({}, '', location.href);
-
-            window.addEventListener('popstate', () => {
-              console.log('onpopstate', urlBackRedirect);
-              setTimeout(() => {
-                location.href = urlBackRedirect;
-              }, 1);
-            });
-          }
-
-          setBackRedirect(link);
-        `}
-      </Script>
+      
       {/* Sales Notification */}
       {showNotification && currentPurchase && (
         <SalesNotification
