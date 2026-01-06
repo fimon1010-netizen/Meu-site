4a9a80511ccffb9cd659968e7f589e8e3d09dabf
Thalyson Alves <fimon1010@gmail.com>
2026-01-06 00:24:12 +0000

    remove static export to fix vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,6 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
 };
 
 export default nextConfig;
